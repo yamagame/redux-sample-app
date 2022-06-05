d840fe4d0e5c96b0c3c78f3ef9d3c2edf1492299
yamagame <yamagame@mac.com>
2022-06-05 11:10:59 +0900

    redux サンプル見直し

--- src/redux-sample/index.js
+++ src/redux-sample/index.js
@@ -1,33 +1,56 @@
 const { createStore, combineReducers } = require("redux");
 
-function page(state = 0, action) {
+const initialCounter1 = { value: 0, tag: "init" };
+
+function counter1(state = initialCounter1, action) {
   switch (action.type) {
-    case "PAGE/INCREMENT":
-      return state + 1;
-    case "PAGE/DECREMENT":
-      return state - 1;
+    case "COUNTER1/INCREMENT":
+      return { value: state.value + 1, tag: action.payload };
+    case "COUNTER1/DECREMENT":
+      return { value: state.value - 1, tag: action.payload };
     default:
       return state;
   }
 }
 
-function content(state = 0, action) {
+const initialCounter2 = { value: 0, tag: "init" };
+
+function counter2(state = initialCounter2, action) {
   switch (action.type) {
-    case "CONTENT/INCREMENT":
-      return state + 1;
-    case "CONTENT/DECREMENT":
-      return state - 1;
+    case "COUNTER2/INCREMENT":
+      return { value: state.value + 1, tag: action.payload };
+    case "COUNTER2/DECREMENT":
+      return { value: state.value - 1, tag: action.payload };
     default:
       return state;
   }
 }
 
-const reducers = combineReducers({ page, content });
+const reducers = combineReducers({ counter1, counter2 });
 const store = createStore(reducers);
 
-store.subscribe(() => console.log(store.getState()));
+// 購読開始
+const unsubscribe1 = store.subscribe(() => {
+  console.log("----- subscribe1");
+  console.log(store.getState());
+});
+const unsubscribe2 = store.subscribe(() => {
+  console.log("----- subscribe2");
+  console.log(store.getState());
+});
 
-store.dispatch({ type: "CONTENT/INCREMENT" });
-store.dispatch({ type: "CONTENT/DECREMENT" });
-store.dispatch({ type: "PAGE/INCREMENT" });
-store.dispatch({ type: "PAGE/DECREMENT" });
+// 配信
+store.dispatch({ type: "COUNTER1/INCREMENT", payload: "step1" });
+store.dispatch({ type: "COUNTER1/DECREMENT", payload: "step2" });
+
+// 購読停止
+unsubscribe1();
+
+// 配信
+store.dispatch({ type: "COUNTER2/INCREMENT", payload: "step3" });
+
+// 購読停止
+unsubscribe2();
+
+// 配信
+store.dispatch({ type: "COUNTER1/INCREMENT", payload: "step4" });
